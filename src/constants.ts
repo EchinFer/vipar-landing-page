2ad3f5b1143dd0e61650a4c6a237ac690f9d8f85
Moochfer <echinfer@gmail.com>
2024-11-13 23:20:26 -0300

    agrega formularios de contacto y suscripción, enlaces de WhatsApp y correo electrónico; mejora el estilo global y se implementa un botón flotante de WhatsApp

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,30 @@
 export const CMS_API_URL = import.meta.env.CMS_API_URL;
 
-export const LOGO_TEXT = 'VIPAR S.A';
+export const LOGO_TEXT = "VIPAR S.A";
+
+export const MENU_ITEMS = [
+  {
+    id: 1,
+    title: "Inicio",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Nosotros",
+    url: "/nosotros/",
+  },
+  {
+    id: 3,
+    title: "Servicios",
+    url: "/servicios/",
+  },
+  {
+    id: 4,
+    title: "Contacto",
+    url: "/contacto/",
+  },
+];
+
+export const telephone = "+595981256510";
+export const email = "info@vipar.com.py";
+export const address = "San Francisco 770 c/ Sexta Bo. Salinas, Ñemby";
