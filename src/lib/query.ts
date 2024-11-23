0e7d1310e4c562e923a42b4c763b296f11411ab6
Moochfer <echinfer@gmail.com>
2024-11-23 14:31:40 -0300

    agrega sección de última obra, mejora el pie de página con enlaces y actualiza el mapa de ubicación; optimiza estilos y componentes

--- src/lib/query.ts
+++ src/lib/query.ts
@@ -1,8 +1,14 @@
-import type { HomePageData } from "@types";
+import type { HomePageData, Project } from "@types";
 import { CMS_API_URL } from "src/constants";
 
 export const getHomePageData = async (): Promise<HomePageData> => {
   return await fetch(`${CMS_API_URL}/custom/v2/home-page`).then((response) =>
+    response.json()
+  );
+};
+
+export const getUltimaObra = async (): Promise<Project> => {
+  return await fetch(`${CMS_API_URL}/custom/v2/last-project`).then((response) =>
     response.json()
   );
 };
