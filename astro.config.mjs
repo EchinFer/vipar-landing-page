b5669f99e26cd0e0391c15915ae47ddff95b74b6
Moochfer <echinfer@gmail.com>
2025-02-17 22:49:50 -0300

    agrega soporte para el adaptador de Vercel y actualiza las dependencias en package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import node from '@astrojs/node';
+import vercel from "@astrojs/vercel";
 import { loadEnv } from "vite";
 
 const { NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(
@@ -10,12 +10,9 @@ const { NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(
   process.cwd(),
   ""
 );
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = NODE_TLS_REJECT_UNAUTHORIZED;
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
 });
